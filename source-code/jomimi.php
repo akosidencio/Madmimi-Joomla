ecace66cdfd7b6fdb991a114a329df4eefe577cb
Dennis Paler <dpaler@shockwebstudio.com>
2014-12-16 15:58:04 +0800

    Updating file structures

---
+++ source-code/jomimi.php
@@ -0,0 +1,54 @@
+<?php
+/*****************************************************************
+ * @package Madmimi - Joomla integration plugin
+ * @version 1.0
+ * @author http://www.shockwebstudio.com
+ * 
+ *****************************************************************/
+
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+
+class plgUserJomimi extends JPlugin
+{
+    /**
+    	 * Example store user method
+    	 *
+    	 * @param	array		$user		Holds the new user data.
+    	 * @param	boolean		$isnew		True if a new user is stored.
+    	 * @param	boolean		$success	True if user was succesfully stored in the database.
+    	 * @param	string		$msg		Message.
+    	 *
+    	 * @return	void
+    	 * @since	1.6
+    	 * @throws	Exception on error.
+    	 */
+        
+    	public function onUserAfterSave($user, $isnew, $success, $msg)
+    	{
+    		
+			$email          = $this->params->get('email');
+			$apikey         = $this->params->get('apikey');
+            $list         = $this->params->get('list');
+			
+			// Split the name for first name and last name based on space
+            $name = explode(' ', $user['name']);
+			
+			
+            // Include Madmimi API
+            require JPATH_SITE . '/plugins/user/jomimi/madmimi-api/MadMimi.class.php';
+            $mailer = new Madmimi($email,$apikey);
+
+            $user = array(
+				'email'=> ($user['email']),
+				'firstName' => $name['0'],
+				'lastName' => $name[1],
+				'add_list' => $list
+			);
+
+            $mailer->AddUser($user);
+    	}
+}
